f8b4a291a4ebb661e79b197563a0680efe08e52d
Krille <c.kussowski@famedly.com>
2024-07-20 10:45:04 +0200

    chore: add tailwindcss build step and basic header design

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/assets");
     return {
         dir: {
             input: "src",
             output: "public"
         }
-    }
-}
+    };
+};
